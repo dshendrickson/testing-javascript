54116d927b3d83a292bc41fbebfa97763a86f1db
Dale S Hendrickson <dale@dshendrickson.com>
2016-08-25 11:21:20 -0600

    Class room exercise code.

--- implementation.js
+++ implementation.js
@@ -2,3 +2,11 @@
 function square(n) {
   return n * n;
 }
+
+function add(a, b) {
+  return a + b;
+}
+
+function modulo(a, b) {
+  return a % b;
+}
--- test.js
+++ test.js
@@ -1,7 +1,30 @@
+// describe('Random Junk', function () {
+//   it('should whatever', function () {
+//     assert.strictequal(true, 1);
+//   });  
+// });
+
 describe('square', function () {
 
   it('returns the square of a number', function () {
     assert.equal(square(2), 4);
   });
 
+});
+
+describe('add', function () {
+
+  it('should return the sum of tow numbers', function () {
+  assert.equal(add(2, 2), 4);
+  assert.equal(add(2, 4), 6);
+  });
+
+});
+
+describe('modulo', function () {
+
+  it('should return a number', function () {
+    assert.isNumber(modulo(2,2));
+  });
+  
 });
